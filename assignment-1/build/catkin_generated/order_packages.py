b849ee1c983e7161dda11b62ad73d42c206c9340
Pulkit Paliwal <20d100021@iitb.ac.in>
2023-01-31 02:21:21 +0530

    created assignment-1 package

---
+++ assignment-1/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/pulkit/sc-627-assignments/assignment-1/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/pulkit/sc-627-assignments/assignment-1/devel;/opt/ros/noetic'.split(';') if '/home/pulkit/sc-627-assignments/assignment-1/devel;/opt/ros/noetic' != '' else []
